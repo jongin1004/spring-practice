44aced46989e7a47db41145ac0d6382a325fb0e1
jongin1004 <bill1224@naver.com>
2023-12-08 15:54:06 +0900

    add test code

--- src/main/java/hello/hellospring/member/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/member/MemoryMemberRepository.java
@@ -1,5 +1,8 @@
 package hello.hellospring.member;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
---
+++ src/test/java/hello/hellospring/member/MemberServiceTest.java
@@ -0,0 +1,24 @@
+package hello.hellospring.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static hello.hellospring.member.Grade.*;
+
+public class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void join() {
+
+        // given
+        Member member = new Member(1L, "memberA", VIP);
+        // when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+        // then
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+
+}
