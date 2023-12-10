4c76746d13e6c11314855d350a8ec1839dbe1aa5
jongin1004 <bill1224@naver.com>
2023-12-10 22:35:37 +0900

    refactoring AppConfig

--- src/main/java/hello/hellospring/AppConfig.java
+++ src/main/java/hello/hellospring/AppConfig.java
@@ -1,19 +1,32 @@
 package hello.hellospring;
 
+import hello.hellospring.discount.DiscountPolicy;
+import hello.hellospring.discount.FixDiscountPolicy;
 import hello.hellospring.discount.RateDiscountPolicy;
+import hello.hellospring.member.MemberRepository;
 import hello.hellospring.member.MemberService;
 import hello.hellospring.member.MemberServiceImpl;
 import hello.hellospring.member.MemoryMemberRepository;
 import hello.hellospring.order.OrderService;
 import hello.hellospring.order.OrderServiceImpl;
+import org.jetbrains.annotations.NotNull;
 
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private static MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public  OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private static DiscountPolicy discountPolicy() {
+//        return new RateDiscountPolicy();
+        return new FixDiscountPolicy();
     }
 }
