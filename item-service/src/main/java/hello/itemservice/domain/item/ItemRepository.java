7c0cb420c118be708d9a989f8f978b95e81c7dfc
jongin1004 <bill1224@naver.com>
2023-12-18 10:34:27 +0900

    create web - add Item repository & test code

---
+++ item-service/src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -0,0 +1,44 @@
+package hello.itemservice.domain.item;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Repository
+public class ItemRepository {
+
+    // Repository는 빈 컨테이너에서 싱글톤으로 관리되기 때문에, 멀티쓰레드에서 동시에 접근을 한다.
+    // HashMap을 사용하면, 동시성 문제가 발생할 수 있으니 ConcurrentHashMap을 사용해야한다.
+    // sequence의 경우도 마찬가지
+    private static final Map<Long, Item> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    public Item save(Item item) {
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+        return item;
+    }
+
+    public Item findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Item> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void update(Long itemId, Item updateParam) {
+        Item findItem = findById(itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
